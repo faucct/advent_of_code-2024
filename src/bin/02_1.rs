74a035f11fe45f37b71887f6f07cfad67ae5f969
Nikita Sokolov <faucct@gmail.com>
2024-12-02 09:09:52 +0100

    02

---
+++ src/bin/02_1.rs
@@ -0,0 +1,50 @@
+fn sum(reader: impl std::io::BufRead) -> usize {
+    reader
+        .lines()
+        .map(|line| line.unwrap())
+        .filter(|line| {
+            if line.is_empty() {
+                return false;
+            }
+            let mut levels = line
+                .split_ascii_whitespace()
+                .map(|level| level.parse::<i32>().unwrap());
+            let start = levels.next().unwrap();
+            let mut prev = levels.next().unwrap();
+            if !(-3..=3).contains(&(prev - start)) || prev == start {
+                return false;
+            }
+            let increasing = prev - start > 0;
+            levels.all(|next| {
+                if !if increasing { 1..=3 } else { -3..=-1 }.contains(&(next - prev)) {
+                    return false;
+                }
+                prev = next;
+                true
+            })
+        })
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        assert_eq!(
+            2,
+            sum("7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"
+                .as_bytes())
+        );
+    }
+}
+
+fn main() {
+    println!("{}", sum(std::io::stdin().lock()));
+}
